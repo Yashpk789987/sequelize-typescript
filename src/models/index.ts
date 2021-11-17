5508f7fd019d948713652afe979940f454bea83a
yashpk789987 <yashpk789987@gmail.com>
2021-11-18 01:13:22 +0530

    done

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,16 +1,5 @@
-import { Sequelize } from "sequelize";
 import { Actor } from "./Actor";
 import { Movie } from "./Movie";
 import { ActorMovie } from "./ActorMovie";
-
-export const sequelize = new Sequelize({
-  dialect: "mysql",
-  username: "root",
-  password: "Node2021",
-  database: "sequelize-typescript",
-  define: {
-    underscored: true,
-  },
-});
 
 export { Movie, Actor, ActorMovie };
